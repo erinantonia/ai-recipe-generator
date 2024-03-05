1746e8fe7fc55225c3419b6fecb47d88e4ac2e82
erinantonia <145416233+erinantonia@users.noreply.github.com>
2024-03-05 15:08:42 +1300

    Added Javascript to handle submit button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Chicken salad 300g chicken 2 tomatoes 2 cups cos lettuce",
+    autoStart: true,
+    delay: 50,
+  });
+}
+
+let ingredientElement = document.querySelector("#ingredient");
+ingredientElement.addEventListener("submit", generateRecipe);
