d700a6448d4941e693d265be5945f21ddeaf534f
erinantonia <145416233+erinantonia@users.noreply.github.com>
2024-03-07 15:46:24 +1300

    Added axios and API, created generateRecipe function in Javascript

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 10,
+  });
+
+  console.log(response);
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#recipe", {
-    strings: "Chicken salad 300g chicken 2 tomatoes 2 cups cos lettuce",
-    autoStart: true,
-    delay: 50,
-  });
+  let ingredientInput = document.querySelector("#ingredient-input");
+  let recipeTitle = document.querySelector("#recipe-title");
+  let recipeIngredients = document.querySelector("#recipe-ingredients");
+
+  let apiKey = "36314taf33204213d293afod0a3b00b2";
+  let prompt = `Please write a short recipe based on the ingredients provided in ${ingredientInput.value}. Please provide a short title to describe the recipe and display it in the <div> ${recipeTitle}, followed by a list of ingredients displayed in the <div> ${recipeIngredients}. Include a maximum of 10 ingredients. After the ingredients, write the directions of the recipe in a clear and concise manner with each step on a new line seperated by a </br>`;
+  let context =
+    "You are a professional recipe creator and experienced chef. You prioritise nutritious, high protein recipes that make use of everyday ingredients and plenty of fresh fruits and vegetables";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
+  console.log("generating recipe");
 }
 
 let ingredientElement = document.querySelector("#ingredient");
